ff1f2375ab6b0d38b1e941e385542def5e981e33
nhs09 <nhs09@DESKTOP-CNC6TOI>
2016-04-14 00:36:13 +0900

    index 페이지 수정 회원가입 폼 수정

--- dvdProject/src/main/java/controller/HelloController.java
+++ dvdProject/src/main/java/controller/HelloController.java
@@ -1,17 +1,12 @@
 package controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@Controller
+/*@Controller
 public class HelloController {
 
 	@RequestMapping("/hello")
-	public String hello(Model model,
-			@RequestParam(value = "name", required = false) String name) {
+	public String hello(Model model, @RequestParam(value = "name", required = false) String name) {
 		model.addAttribute("greeting", "안녕하세요, " + name);
 		return "hello";
 	}
 }
+*/
