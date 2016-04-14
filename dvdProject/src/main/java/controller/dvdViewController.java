7bb2085fa4e320d053a45c56ad3242bce88518a7
nhs0912 <nhs09@DESKTOP-CNC6TOI>
2016-04-14 19:42:17 +0900

    Upload file dvdViewController.java

---
+++ dvdProject/src/main/java/controller/dvdViewController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class dvdViewController {
+
+	@RequestMapping("/dvdView")
+	public String dvdView()
+	{
+		System.out.println("작동");
+		return "/dvdView";
+	}
+}
